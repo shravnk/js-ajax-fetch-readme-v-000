83d16d5efee994714b8a8e2c8d0db9a33443677b
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-21 04:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = 'c20d3ba0828c74aa4b4fd673416d381b5fa1a4a3'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
